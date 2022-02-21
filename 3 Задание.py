9b7fe4fd38e437d8824334aa18a6f7121bb0ab00
wano1234 <99421271+wano1234@users.noreply.github.com>
2022-02-21 18:31:15 +0300

    Add files via upload

--- 3 Задание.py
+++ 3 Задание.py
@@ -1,11 +1,25 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369
-
-n = int(input('Введите число? '))
-temp = str(n)
-t1 = temp + temp
-t2 = temp + temp + temp
-comp = n + int(t1) + int(t2)
-print('Ответ:', comp)
-
-
+# Задача-3: Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+number = int(input("Enter month number: "))
+if number <= 12 and number >= 1:
+    month_dict = {1: 'January',
+                  2: 'February',
+                  3: 'March',
+                  4: 'April',
+                  5: 'May',
+                  6: 'June',
+                  7: 'Jule',
+                  8: 'August',
+                  9: 'September',
+                  10: 'October',
+                  11: 'November',
+                  12: 'December'}
+    month_list = list(month_dict.values())
+    for i, el in enumerate(month_list):
+        if i == number-1:
+            print(f"Month from list is {month_list[i]}")
+            break
+    print(f"Month from dict is {month_dict[number]}")
+else:
+    print("You made a mistake")
